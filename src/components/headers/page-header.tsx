cce2ceed6c82b5fbeb57d8f8bd20a35531fe0be5
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-11 07:49:25 -0300

    feat: add page header

---
+++ src/components/headers/page-header.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { Stack, Typography } from "@mui/material";
+import { WooviLogo } from "../../components";
+
+interface PageHeaderI {
+  title: string;
+}
+
+export const PageHeader: FC<PageHeaderI> = ({ title }) => {
+  return (
+    <Stack alignItems="center" spacing={4}>
+      <WooviLogo
+        sx={{
+          Width: "123px",
+          Height: "auto",
+          mx: "auto",
+        }}
+      />
+      <Typography
+        variant="heading1"
+        textAlign="center"
+        color="neutral.dark"
+        px={[3, 0]}
+      >
+        {title}
+      </Typography>
+    </Stack>
+  );
+};
