7964592ec951df0afc8a2159626621c762ae8a70
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-11 07:48:41 -0300

    feat: add page router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts";
+import { PaymentMethod } from "./pages";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<PaymentMethod />} />
+      </Route>
     </Routes>
   );
 };
