fa9688640d016e417156b479fb8e0d755ab63a5b
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-14 05:20:17 -0300

    feat: add payment-options-store

---
+++ src/stores/payment-options.store.ts
@@ -0,0 +1,19 @@
+import { createStore } from "./index";
+import { PIX_OPTIONS as paymentOptions } from "../helpers";
+import { PixOptionsType } from "../interfaces";
+import { formatCurrency } from "../utils";
+
+const mapOptions = (options: PixOptionsType) => {
+  return options.map((option) => ({
+    ...option,
+    amount: formatCurrency({ value: option.amount }),
+    total: formatCurrency({ value: option.total || 0 }),
+  }));
+};
+
+const formattedOptions = mapOptions(paymentOptions);
+
+export const paymentOptionsStore = createStore({
+  options: formattedOptions,
+  selectedOption: formattedOptions[1],
+});
