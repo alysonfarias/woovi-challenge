178ef6f6f3e0a493585ff8d3da355393e6f3de20
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-14 15:13:59 -0300

    fix: main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ThemeProvider } from "@mui/material";
-import { lightTheme } from "./themes/light-theme.ts";
+import App from "./App";
+import { lightTheme } from "./themes/light-theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
