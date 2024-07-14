eba241a60f815757d749c4378e6baa48b05c23b4
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-14 15:06:28 -0300

    feat: add credit card

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts";
-import { PaymentMethod } from "./pages";
+import { PaymentMethod, CreditCard, Pix } from "./pages";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<PaymentMethod />} />
+        <Route path="/pix" element={<Pix />} />
+        <Route path="/credit-card" element={<CreditCard />} />
       </Route>
     </Routes>
   );
